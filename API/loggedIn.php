7de0124a5b72e5c7d0e6c2466360a5a811a7afe2
CasiKea <casianoros1223@gmail.com>
2017-03-29 18:05:01 +0200

    login register adn population solved

--- API/loggedIn.php
+++ API/loggedIn.php
@@ -2,12 +2,11 @@
 
 	session_start();
 
-	$_SESSION["userData"] = '{"status":"success","fname":"casi","lname":"oros","pass":"123","email":"casianoros1223@gmail.com","adress":"Copenhagen"}';
-
 	if( isset( $_SESSION["userData"] ) ){
 		echo $_SESSION["userData"];
 	}else{
-		echo '{"status":"error"}';
+		$_SESSION["userData"] = '{"status":"error"}';
+		echo $_SESSION["userData"];
 	}
 	
 ?>
--- API/login.php
+++ API/login.php
@@ -2,17 +2,41 @@
 
 session_start();
 
-$userId = "Casian";
-$userPass = "Oros";
+
 $userInput = $_GET["userData"];
+// file_put_contents("../JSON/testing.json", $userInput);
 $jUserData = json_decode($userInput);
 
-if($jUserData->name == $userId && $jUserData->pass == $userPass){
-	$_SESSION["userData"] = $userInput;
-	echo '{"status":"ok"}';
-}else{
-	echo '{"status":"error"}';
-	session_destroy();
-}
+$sArrayOfUsers = file_get_contents("../JSON/test.json");
+$arrayOfUsers = json_decode($sArrayOfUsers);
+
+// file_put_contents("../JSON/testing.json", $jUserData->name);
+
+for ($i=0; $i < count($arrayOfUsers); $i++) { 
+
+	if($arrayOfUsers[$i]->fname == $jUserData->name && $arrayOfUsers[$i]->pass == $jUserData->pass){
+		$stringConv = json_encode($arrayOfUsers[$i]);
+		file_put_contents("../JSON/testing.json", $stringConv);
+		$_SESSION["userData"] = $stringConv;
+		echo $stringConv;
+		break;
+	}else{
+		echo '{"status":"error"}';
+		session_destroy();
+		file_put_contents("../JSON/testing.json", "nope");
+	};
+
+	// if ($arrayOfUsers[$i]->fname == $jUserData->name && $arrayOfUsers[$i]->pass == $jUserData->pass){
+	// 	file_put_contents("../JSON/testing.json", "yes");
+	// 	// $sUser = json_encode($arrayOfUsers[$i]);
+	// 	// echo $sUser;
+	// 	// $_SESSION["userData"] = $sUser;
+	// }else{
+	// file_put_contents("../JSON/testing.json", "nope");	
+	// // echo '{"status":"error"}';
+	// // session_destroy();
+	// };
+};
+
 
 ?>
